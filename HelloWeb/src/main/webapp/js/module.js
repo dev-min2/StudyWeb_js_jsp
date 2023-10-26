ca6eda5396ebf31b02e3a9ca58f07e83a5d0c2e9
minky22 <c11286@naver.com>
2023-10-26 16:55:49 +0900

    3일차 객체, 문자열등

---
+++ HelloWeb/src/main/webapp/js/module.js
@@ -0,0 +1,10 @@
+/**
+ * 
+ */ 
+
+import {friend, friendInfo} from './friend.js';
+import { cal } from '../todo/CalendarObj.js';
+
+console.log(friend.showInfo());
+console.log(friendInfo(friend));
+cal.showCalendar();
